0dc19c9797a00f1942c17f32a3a9ff21ac09067c
DamianBasista <damian.basista@gmail.com>
2018-05-22 08:59:50 +0200

    make clock

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ setInterval(function(){
     var date = new Date();
 
     var format = [
-        (date.getHours(),
-         date.getMinutes(),
-         date.getSeconds())
-    ]
+        (date.getHours()),
+        (date.getMinutes()),
+        (date.getSeconds())
+    ].join(":");
     document.getElementById('clock').innerHTML = format;
-}) 
+}, 1000); 
