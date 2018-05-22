ada342e9dfab310a0c2bbbabcde685dd5f3978de
DamianBasista <damian.basista@gmail.com>
2018-05-22 10:22:38 +0200

    add '0' to the clock

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ setInterval(function(){
     var date = new Date();
 
     var format = [
-        (date.getHours()),
-        (date.getMinutes()),
-        (date.getSeconds())
+        (date.getHours() <10 ? "0" + date.getHours() : date.getHours()),
+        (date.getMinutes() <10 ? "0" + date.getMinutes() : date.getMinutes()),
+        (date.getSeconds() <10 ? "0" + date.getSeconds() : date.getSeconds())
     ].join(":");
     document.getElementById('clock').innerHTML = format;
 }, 1000); 
