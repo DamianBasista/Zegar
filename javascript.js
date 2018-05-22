a5419e614282af19bb766acaa1f9624719537434
DamianBasista <damian.basista@gmail.com>
2018-05-22 08:50:47 +0200

    add 'id=clock'

--- javascript.js
+++ javascript.js
@@ -2,8 +2,9 @@ setInterval(function(){
     var date = new Date();
 
     var format = [
-        (date.getHours();
-         date.getMinutes();
-         date.getSeconds();)
+        (date.getHours(),
+         date.getMinutes(),
+         date.getSeconds())
     ]
+    document.getElementById('clock').innerHTML = format;
 }) 
